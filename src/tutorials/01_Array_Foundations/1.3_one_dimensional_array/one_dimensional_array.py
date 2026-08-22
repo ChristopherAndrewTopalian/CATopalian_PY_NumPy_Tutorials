c15645dec5ee25e297de08d0aa3049d2c5193ff3
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-08-22 12:05:14 -0400

    update

---
+++ src/tutorials/01_Array_Foundations/1.3_one_dimensional_array/one_dimensional_array.py
@@ -0,0 +1,23 @@
+# one_dimensional_array.py
+
+import numpy as np
+
+# we make a one dimensional array
+our_array = np.array([10, 17, 41, 19, 16, 8])
+
+print(our_array)
+
+####
+
+'''
+[10 17 41 19 16  8]
+'''
+
+####
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
+# https://github.com/ChristopherAndrewTopalian
+# https://github.com/ChristopherTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
